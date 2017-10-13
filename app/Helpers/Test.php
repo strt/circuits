50837f2297c2da271dbab15315036981650e44b3
Alexander Karlsson <alexander.karlsson@strateg.se>
2017-10-13 15:19:42 +0200

    Add Index twig.

---
+++ app/Helpers/Test.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Helpers;
+
+class Test
+{
+    public function __construct()
+    {
+        echo 'bam';
+    }
+
+    public function dostuff() {
+        echo 'doing stuff';
+    }
+}
