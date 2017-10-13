c83483b3232a2599f437cb8a23412cf2a23339c1
Alexander Karlsson <alexander.karlsson@strateg.se>
2017-10-13 15:19:11 +0200

    Update Return container instance if no abstract is given.

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -6,8 +6,12 @@ use Illuminate\Container\Container;
  * This will be moved into library in future.
  *
  **/
-function app($abstract, $data = []) {
+function app($abstract = null, $data = []) {
     $container = Container::getInstance();
+
+    if (is_null($abstract)) {
+        return $container;
+    }
 
     return empty($data)
         ? $container->make($abstract)
@@ -19,6 +23,7 @@ function config($name, $default = null) {
 }
 
 function view($template, $data = []) {
-    $template = app('twig')->load($template);
+    $template = str_replace('.', '/', $template);
+    $template = app('twig')->load("{$template}.twig");
     return $template->render($data);
 }
