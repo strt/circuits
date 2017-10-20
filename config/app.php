267bc31e6740b9b2190953a05776d53168919215
Alexander Karlsson <alexander.karlsson@strateg.se>
2017-10-20 17:16:40 +0200

    Woops

--- config/app.php
+++ config/app.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Config data for the application
+ * 
+ * @author Alexander Karlsson <alexander.karlsson@strateg.se>
+ * 
+ * @return array
+ */
 return [
     'name' => 'Hehe'
 ];
--- resources/functions.php
+++ resources/functions.php
@@ -2,7 +2,18 @@
 
 require_once dirname(__DIR__) . '/bootstrap/app.php';
 
-// Setup where to find our theme.
+/**
+ * Set hook on wordpress functions and modifies the path returned to current dir.
+ * 
+ * @author Alexander Karlsson <alexander.karlsson@strateg.se>
+ * 
+ * @link https://developer.wordpress.org/reference/hooks/theme_file_path/ theme_file_path hook
+ * @link https://developer.wordpress.org/reference/hooks/theme_file_uri/ theme_file_uri hook
+ * @link https://developer.wordpress.org/reference/hooks/parent_theme_file_path/ parent_theme_file_path hook
+ * @link https://developer.wordpress.org/reference/hooks/parent_theme_file_uri/ parent_theme_file_uri hook
+ * 
+ * @return array
+ */
 array_map(
     'add_filter',
     ['theme_file_path', 'theme_file_uri', 'parent_theme_file_path', 'parent_theme_file_uri'],
