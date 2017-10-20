4e4bb1fecb411d21ec543381e43be08a8fcd16c1
Jonas Lilja <jonas@lilja.io>
2017-10-20 16:53:47 +0200

    added documentation

--- resources/functions.php
+++ resources/functions.php
@@ -4,14 +4,11 @@ require_once dirname(__DIR__) . '/bootstrap/app.php';
 
 /**
  * Set hook on wordpress functions and modifies the path returned to current dir.
- * 
  * @author Alexander Karlsson <alexander.karlsson@strateg.se>
- * 
  * @link https://developer.wordpress.org/reference/hooks/theme_file_path/ theme_file_path hook
  * @link https://developer.wordpress.org/reference/hooks/theme_file_uri/ theme_file_uri hook
  * @link https://developer.wordpress.org/reference/hooks/parent_theme_file_path/ parent_theme_file_path hook
  * @link https://developer.wordpress.org/reference/hooks/parent_theme_file_uri/ parent_theme_file_uri hook
- * 
  * @return array
  */
 array_map(
